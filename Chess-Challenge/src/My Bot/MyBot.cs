643442146794e1e05da6f71115b030f3378debf6
D1EG026 <diegodezaragoza@gmail.com>
2025-04-13 22:44:07 +0200

    Update MyBot.cs

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -7,6 +7,7 @@ using ChessChallenge.Application;
 public class MyBot : IChessBot
 {
     int[] pieceValues = { 0, 100, 300, 300, 500, 900, 10000 };
+    //ulong fileA=;
     public Move Think(Board board, Timer timer)
     {
         Move[] legalMoves = board.GetLegalMoves();
@@ -15,48 +16,58 @@ public class MyBot : IChessBot
         Move bestMove = legalMoves[0];
         foreach (var move in legalMoves)
         {
+            board.MakeMove(move);
             float moveScore = GetImmediateScore(board);
             evaluatedMoves.Add(new(move, moveScore)); // O calcular otra vez la puntuacion solo al final y asi no se guarda tambien en diccionario
             if (moveScore > maxScore)
                 maxScore = moveScore;
+            board.UndoMove(move);
         }
         bestMove = evaluatedMoves.First(x => x.Value == maxScore).Key;
         //bestMove = evaluatedMoves.Aggregate((x, y) => x.Value > y.Value ? x : y).Key;
-        bestMove = legalMoves[0];
+        foreach (var move in evaluatedMoves)
+        {
+            Console.WriteLine(move.Key + " " + move.Value);
+        }
+        Console.WriteLine("Best move: " + bestMove + " with score: " + maxScore);
         return bestMove;
     }
-    float score = 0;
+
     float GetImmediateScore(Board board)
     {
-        score = 0;
+        float score = 0;
         foreach (var move in board.GetLegalMoves())
         {
-            board.MakeMove(move);
-            float materialScore = GetMaterialScore(board.IsWhiteToMove, board);
+            //board.MakeMove(move);
+            //float materialScore = GetMaterialScore(board.IsWhiteToMove, board);
             float positionalScore = GetPositionalScore(board.IsWhiteToMove, board);
             score = positionalScore;
-            board.UndoMove(move);
+            if (move.MovePieceType == PieceType.Pawn) score = 1;
+            //board.UndoMove(move);
         }
-        Console.WriteLine("Score: " + score);
         return score;
     }
 
     private float GetPositionalScore(bool isWhiteToMove, Board board)
     {
+        float score = 0;
         //derecha x>>
         //izquierda x<<
         //arriba x>>8
         //abajo x<<8
-        foreach (var pawn in board.GetPieceList(PieceType.Pawn, isWhiteToMove))
-        {
-            ulong pawnBitBoard = (ulong)Math.Pow(2, pawn.Square.Index);
 
-            // if ((((pawnBitBoard << 1) >> 8) & board.GetPieceBitboard(PieceType.Knight, isWhiteToMove)) > 1)
-            if ((AddDirection(pawnBitBoard, 1, 1, isWhiteToMove) & board.GetPieceBitboard(PieceType.Knight, isWhiteToMove)) > 1)
-                // si el peon esta protegiendo otra pieza
-                score += 1;
-        }
-        
+
+        ulong pawnsBitBoard = board.GetPieceBitboard(PieceType.Pawn, isWhiteToMove);
+        //ulong pawnAttackRight = pawnsBitBoard
+        // foreach (var pawn in board.GetPieceList(PieceType.Pawn, isWhiteToMove))
+        // {
+        //     //ulong pawnBitBoard = Convert.To((ulong)Math.Pow(2, pawn.Square.Index),2);
+
+        //     if ((pawnBitBoard.AddDirection(-1, 1, isWhiteToMove) & board.GetPieceBitboard(PieceType.Knight, isWhiteToMove)) > 1)
+        //         // si el peon esta protegiendo otra pieza
+        //         score += 1;
+        // }
+
         // board.GetPieceBitboard(PieceType.Pawn, isWhiteToMove);
         // foreach (var knight in board.GetPieceList(PieceType.Knight, isWhiteToMove))
         // {
@@ -73,15 +84,6 @@ public class MyBot : IChessBot
         //     }
         // }
         return score;
-    }
-    ulong AddDirection(ulong input, int x, int y, bool isWhite)
-    {
-        if (!isWhite)
-        {
-            x = -x;
-            y = -y;
-        }
-        return input + (ulong)Math.Pow(2, x + y * 8);
     }
 
     int GetMaterialScore(bool isWhite, Board board)
@@ -101,5 +103,17 @@ public class MyBot : IChessBot
             }
         }
         return score;
+    }
+}
+public static class Extensions
+{
+    public static ulong AddDirection(this ulong input, int x, int y, bool isWhite)
+    {
+        if (!isWhite)
+        {
+            x = -x;
+            y = -y;
+        }
+        return input + (ulong)Math.Pow(2, x + y * 8);
     }
 }
